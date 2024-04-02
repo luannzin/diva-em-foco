5307e9d2cacc2495d2e170fde534389f43f6d360
luannzin <luandaniel966@gmail.com>
2024-04-02 16:31:21 -0300

    feat: add new link to recent posts

--- app/page.tsx
+++ app/page.tsx
@@ -18,30 +18,33 @@ export default async function Home() {
   return (
     <div>
       <main>
-        <div className="flex items-center w-full h-[600px] bg-rose-500 rounded-lg">
-          <div className="flex flex-col justify-end bg-rose-200 w-full h-full rounded-sm p-8 gap-4">
-            <Link
-              href={
-                headerLinks.find((link) => link.tag === data[0].category) ?? "/"
-              }
-              className="p-2 bg-rose-400 text-rose-50 rounded-sm w-max text-sm uppercase select-none"
-            >
-              {data[0].category || "Sem categoria"}
-            </Link>
-            <div className="flex flex-col gap-2 text-rose-950">
-              <strong className="text-3xl ">{data[0].title}</strong>
-              <span>
-                {data[0].content.length > 64
-                  ? `${data[0].content.slice(0, 64)}...`
-                  : data[0].content}
-              </span>
+        <Link href={`/post/${data[0].slug}`}>
+          <div className="flex items-center w-full h-[600px] bg-rose-500 rounded-lg">
+            <div className="flex flex-col justify-end bg-rose-200 w-full h-full rounded-sm p-8 gap-4">
+              <Link
+                href={
+                  headerLinks.find((link) => link.tag === data[0].category) ??
+                  "/"
+                }
+                className="p-2 bg-rose-400 text-rose-50 rounded-sm w-max text-sm uppercase select-none"
+              >
+                {data[0].category || "Sem categoria"}
+              </Link>
+              <div className="flex flex-col gap-2 text-rose-950">
+                <strong className="text-3xl ">{data[0].title}</strong>
+                <span>
+                  {data[0].content.length > 64
+                    ? `${data[0].content.slice(0, 64)}...`
+                    : data[0].content}
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col w-full h-full rounded-sm">
+              <div className="flex bg-rose-300 h-full w-full"></div>
+              <div className="flex bg-rose-400 h-full w-full"></div>
             </div>
           </div>
-          <div className="flex flex-col w-full h-full rounded-sm">
-            <div className="flex bg-rose-300 h-full w-full"></div>
-            <div className="flex bg-rose-400 h-full w-full"></div>
-          </div>
-        </div>
+        </Link>
         {/* {data && data.length > 0 ? (
           data.map(
             (post: {
